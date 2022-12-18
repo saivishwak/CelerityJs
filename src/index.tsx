41d45da61f9e3bdba944ce306e121fa33a22acd9
saivishwak <saivishwak40@gmail.com>
2022-12-18 17:09:00 +0530

    Small clean up

--- src/index.tsx
+++ src/index.tsx
@@ -48,41 +48,6 @@ const Celerity = {
         return element;
     },
 
-    /*renderDom: (element: FrameworkInterface, container: any): void => {
-        console.log("Element", element);
-        if (!element.tag) return;
-
-        if (typeof element.tag == "function") {
-            return Celerity.render(
-                element.tag() as FrameworkInterface,
-                container
-            );
-        }
-
-        const dom = document.createElement(element.tag);
-        const isEvent = (key) => key.startsWith("on");
-        const isProperty = (key) => key !== "children";
-        Object.keys(element.props)
-            .filter(isProperty)
-            .forEach((name) => {
-                if (isEvent(name)) {
-                    dom.addEventListener(
-                        element.props[name].name,
-                        element.props[name]
-                    );
-                } else {
-                    dom[name] = element.props[name];
-                }
-            });
-        element.props.children.forEach((child) => {
-            if (typeof child !== "object") {
-                const textNode = document.createTextNode(child);
-                dom.appendChild(textNode);
-            } else Celerity.render(child, dom);
-        });
-        container.appendChild(dom);
-    },
-    */
     createDom: (fiber: any): any => {
         const dom =
             fiber.type == "TEXT_ELEMENT"
